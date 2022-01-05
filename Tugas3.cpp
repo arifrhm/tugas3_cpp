7714fd7556f5efc69cb65c90fabc6d0305d78631
arifrhm <96586737+arifrhm@users.noreply.github.com>
2022-01-05 23:07:06 +0700

    Add files via upload

---
+++ Tugas3.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+int a = 10;
+int b = 20;
+  cout << "Nilai Variabel A : "<< a << "\n";
+  cout << "Nilai Variabel B : "<< b << "\n";
+  cout << "Hasil Perkalian Kedua Variabel Adalah : " << a*b <<"\n";
+  return 0;
+}
